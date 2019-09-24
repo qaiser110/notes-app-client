f5a3bf3ca82f89958fd34710fec1e81542584e6d
Qaiser Abbas <Abbas.Qaiser@abc.net.au>
2019-09-24 19:09:52 +1000

    Call the List API
    https://serverless-stack.com/chapters/call-the-list-api.html

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { PageHeader, ListGroup } from "react-bootstrap";
+import { API } from "aws-amplify";
+import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import "./Home.css";
 
 export default class Home extends Component {
@@ -12,8 +14,48 @@ export default class Home extends Component {
     };
   }
 
+  async componentDidMount() {
+    if (!this.props.isAuthenticated) {
+      return;
+    }
+
+    try {
+      const notes = await this.notes();
+      this.setState({ notes });
+    } catch (e) {
+      alert(e);
+    }
+
+    this.setState({ isLoading: false });
+  }
+
+  notes() {
+    return API.get("notes", "/notes");
+  }
+
   renderNotesList(notes) {
-    return null;
+    return [{}].concat(notes).map(
+      (note, i) =>
+        i !== 0
+          ? <LinkContainer
+            key={note.noteId}
+            to={`/notes/${note.noteId}`}
+          >
+            <ListGroupItem header={note.content.trim().split("\n")[0]}>
+              {"Created: " + new Date(note.createdAt).toLocaleString()}
+            </ListGroupItem>
+          </LinkContainer>
+          : <LinkContainer
+            key="new"
+            to="/notes/new"
+          >
+            <ListGroupItem>
+              <h4>
+                <b>{"\uFF0B"}</b> Create a new note
+              </h4>
+            </ListGroupItem>
+          </LinkContainer>
+    );
   }
 
   renderLander() {

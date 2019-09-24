e0bb400591925a8500b36d14dac653a5f311acb7
Qaiser Abbas <Abbas.Qaiser@abc.net.au>
2019-09-24 12:50:35 +1000

    Signup / Confirm Code
    https://serverless-stack.com/chapters/signup-with-aws-cognito.html

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Auth } from "aws-amplify";
 import {
   HelpBlock,
   FormGroup,
@@ -45,7 +46,17 @@ export default class Signup extends Component {
 
     this.setState({ isLoading: true });
 
-    this.setState({ newUser: "test" });
+    try {
+      const newUser = await Auth.signUp({
+        username: this.state.email,
+        password: this.state.password
+      });
+      this.setState({
+        newUser
+      });
+    } catch (e) {
+      alert(e.message);
+    }
 
     this.setState({ isLoading: false });
   }
@@ -54,6 +65,17 @@ export default class Signup extends Component {
     event.preventDefault();
 
     this.setState({ isLoading: true });
+
+    try {
+      await Auth.confirmSignUp(this.state.email, this.state.confirmationCode);
+      await Auth.signIn(this.state.email, this.state.password);
+
+      this.props.userHasAuthenticated(true);
+      this.props.history.push("/");
+    } catch (e) {
+      alert(e.message);
+      this.setState({ isLoading: false });
+    }
   }
 
   renderConfirmationForm() {

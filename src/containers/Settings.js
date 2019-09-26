5088325447fc0442ae12b1e3cce173717ccf347b
Qaiser Abbas <Abbas.Qaiser@abc.net.au>
2019-09-26 12:59:38 +1000

    Connecting the billing form to Stripe SDK
    https://serverless-stack.com/chapters/connect-the-billing-form.html

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import { API } from "aws-amplify";
+import { Elements, StripeProvider } from "react-stripe-elements";
+import BillingForm from "../components/BillingForm";
+import config from "../config";
+import "./Settings.css";
 
 export default class Settings extends Component {
   constructor(props) {
@@ -16,9 +20,42 @@ export default class Settings extends Component {
     });
   }
 
+/*
+We are adding the BillingForm component that we previously created here and passing in the loading and onSubmit prop that we referenced in the last chapter. In the handleFormSubmit method, we are checking if the Stripe method from the last chapter returned an error. And if things looked okay then we call our billing API and redirect to the home page after letting the user know.
+*/
+  handleFormSubmit = async (storage, { token, error }) => {
+    if (error) {
+      alert(error);
+      return;
+    }
+
+    this.setState({ isLoading: true });
+
+    try {
+      await this.billUser({
+        storage,
+        source: token.id
+      });
+
+      alert("Your card has been charged successfully!");
+      this.props.history.push("/");
+    } catch (e) {
+      alert(e);
+      this.setState({ isLoading: false });
+    }
+  }
+
   render() {
     return (
       <div className="Settings">
+        <StripeProvider apiKey={config.STRIPE_KEY}>
+          <Elements>
+            <BillingForm
+              loading={this.state.isLoading}
+              onSubmit={this.handleFormSubmit}
+            />
+          </Elements>
+        </StripeProvider>
       </div>
     );
   }
